3674e66f50e1a54d79a9fdae9422aee49d2b5422
alchogh <alchogh01@gmail.com>
2024-06-05 15:57:00 +0900

    🎨 intersaction observer 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef(null);
+  const options = {
+    threshold: 0.5,
+  };
+  const observer = new IntersectionObserver((entries) => {
+    if (entries[0].isIntersecting) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  }, options);
+
+  useEffect(() => {
+    if (ref.current) {
+      observer.observe(ref.current);
+    } else {
+      console.error(" not found");
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="content" id="content1">
+        <span>콘텐츠 1</span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="content" id="content2">
+        <span>콘텐츠 2</span>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div
+        className={`content ${isVisible ? "visible" : ""}`}
+        id="content3"
+        ref={ref}
+      >
+        <span>콘텐츠 3</span>
+      </div>
+      <div className="content" id="content4">
+        <span>콘텐츠 4</span>
+      </div>
+      <div className="content" id="content5">
+        <span>콘텐츠 5</span>
+      </div>
+      <div className="content" id="content6">
+        <span>콘텐츠 6</span>
+      </div>
+      <div className="content" id="content7">
+        <span>콘텐츠 7</span>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
